af283bc2a5e3b74ecf7961260cbc4f32ac1eca76
ardy-priyo <priyo.ardy@hotmail.com>
2025-08-19 14:06:24 +0700

    Preparing to processing auth process

---
+++ app/Controllers/Auth/Auth.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Auth extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
