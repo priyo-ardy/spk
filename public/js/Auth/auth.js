42aaa67655e990a648f3f8e3a494675386429bc0
ardy-priyo <priyo.ardy@hotmail.com>
2025-10-01 16:10:38 +0700

    Update add SPK

--- public/js/Auth/auth.js
+++ public/js/Auth/auth.js
@@ -73,6 +73,11 @@ function resetForm() {
   dataForm.user_name.focus();
 }
 
+buttons.auth.addEventListener("click", function () {
+  if (validasi()) {
+    prosesLogin();
+  }
+});
 async function prosesLogin() {
   if (validasi()) {
     try {
